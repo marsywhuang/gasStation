73b21b6e1c7d3fe467cb80d2c7abc31ac96d290a
marsywhuang <s939703@gmail.com>
2018-10-09 05:23:46 +0800

    Create parserTranDelt_v1.py

---
+++ parserTranDelt_v1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+print (sys.argv[1])
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 載入函式庫
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+
+# 定義資料結構
+schema = StructType([
+  StructField("Deptno", StringType(), True),
+  StructField("Island", StringType(), True),
+  StructField("Gun_No", StringType(), True),
+  StructField("Tran_Time", TimestampType(), True),
+  StructField("Seq", StringType(), True),
+  StructField("Tax_Type", StringType(), True),
+  StructField("Product_ID", StringType(), True),
+  StructField("Class", StringType(), True),
+  StructField("Price", FloatType(), True),
+  StructField("Amt", FloatType(), True),
+  StructField("Qty", FloatType(), True),
+  StructField("Unit", StringType(), True),
+  StructField("Ref_No", StringType(), True)
+])
+
+# 讀入原始資料，使用預先設定結構
+dfTranDetl = sqlContext.read.csv("/home/mywh/data/tran_detl.csv", header=True, schema=schema)
+
+#
+datetimeRange = str(sys.argv[1])
+datetimeFrom = datetimeRange + " " + "00:00:00"
+datetimeTo = datetimeRange + " " + "23:59:59"
+tmpDfTranDelt = dfTranDetl.where((dfTranDetl["Tran_Time"] >= datetimeFrom) & (dfTranDetl["Tran_Time"] <= datetimeTo))
+
+# 寫入檔案、多檔模式、CSV 格式
+outputPath = "/home/mywh/data"
+outputFileName = datetimeRange + "_TranDelt.csv"
+tmpDfTranDelt.write.csv(outputPath + "/" + outputFileName)
