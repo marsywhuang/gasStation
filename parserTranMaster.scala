f50c4361b02542d3c37ab6eb3ff69e51d71665e3
marsywhuang <s939703@gmail.com>
2018-10-14 05:31:52 +0800

    Create parserTranMaster.scala

---
+++ parserTranMaster.scala
@@ -0,0 +1,5 @@
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+val df = sqlContext.read.format("csv").option("header", "true").load("/Users/apple/data/tran_master.csv")
+val gDf = tmpDf.groupBy("Deptno", "Tran_time", "Payment").agg(collect_list(struct("Deptno", "Tran_time", "Payment"))).alias("message") 
+val filterDf = df.filter($"Tran_time".contains("2018-02-01"))
+val tmpDf.collect().foreach(println)
