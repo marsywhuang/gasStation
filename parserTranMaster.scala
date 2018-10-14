97be232e1bbf2d8c6b43b275dec715d4da53f341
marsywhuang <s939703@gmail.com>
2018-10-14 22:16:21 +0800

    Update parserTranMaster.scala

--- parserTranMaster.scala
+++ parserTranMaster.scala
@@ -1,6 +1,11 @@
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val df = sqlContext.read.format("csv").option("header", "true").load("/Users/apple/data/tran_master.csv")
 
+
+val dtYear : List[String] = List("2018")
+val dtMonth : List[String] = List("01")
+val dtDay : List[String] = List("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")
+
 for (idxYear <- 0 to (dtYear.length - 1)) {
   for (idxMonth <- 0 to (dtMonth.length - 1)) {
     for (idxDay <- 0 to (dtDay.length - 1)) {
