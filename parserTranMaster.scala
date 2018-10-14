edb141759e1a0cf1683bc6f2ec2a053b6ef0c6ff
marsywhuang <s939703@gmail.com>
2018-10-14 22:12:42 +0800

    Update parserTranMaster.scala

--- parserTranMaster.scala
+++ parserTranMaster.scala
@@ -1,8 +1,20 @@
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val df = sqlContext.read.format("csv").option("header", "true").load("/Users/apple/data/tran_master.csv")
 
-val filterDf = df.filter($"Tran_time".contains("2018-02-01"))
-filterDf.write.csv("/Users/apple/data/tranMaster_20180202.csv")
+for (idxYear <- 0 to (dtYear.length - 1)) {
+  for (idxMonth <- 0 to (dtMonth.length - 1)) {
+    for (idxDay <- 0 to (dtDay.length - 1)) {
+      print(dtYear(idxYear) + "-" + dtMonth(idxMonth) + "-" + dtDay(idxDay))
+      print("\n")
+      //
+      val dtRange = dtYear(idxYear) + "-" + dtMonth(idxMonth) + "-" + dtDay(idxDay)
+      val dtFileName = "/Users/apple/data/" + dtYear(idxYear) + dtMonth(idxMonth) + dtDay(idxDay) + ".csv"
+      //
+      val filterDf = df.filter($"Tran_time".contains(dtRange))
+      filterDf.write.csv(dtFileName)
+    }
+  }
+}
 
 val gDf = tmpDf.groupBy("Deptno", "Tran_time", "Payment").agg(collect_list(struct("Deptno", "Tran_time", "Payment"))).alias("message") 
 val tmpDf.collect().foreach(println)
