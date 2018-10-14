d80ef5b3c048e1d972bce4076cbf4f3e22f98e04
marsywhuang <s939703@gmail.com>
2018-10-14 19:06:53 +0800

    Update parserTranMaster.scala

--- parserTranMaster.scala
+++ parserTranMaster.scala
@@ -1,5 +1,8 @@
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val df = sqlContext.read.format("csv").option("header", "true").load("/Users/apple/data/tran_master.csv")
-val gDf = tmpDf.groupBy("Deptno", "Tran_time", "Payment").agg(collect_list(struct("Deptno", "Tran_time", "Payment"))).alias("message") 
+
 val filterDf = df.filter($"Tran_time".contains("2018-02-01"))
+filterDf.write.csv("/Users/apple/data/tranMaster_20180202.csv")
+
+val gDf = tmpDf.groupBy("Deptno", "Tran_time", "Payment").agg(collect_list(struct("Deptno", "Tran_time", "Payment"))).alias("message") 
 val tmpDf.collect().foreach(println)
