e3dd765bae9060591bb68e16bd3787fc8e19e90a
marsywhuang <s939703@gmail.com>
2018-10-18 05:46:29 +0800

    Update parserTranMaster.scala

--- parserTranMaster.scala
+++ parserTranMaster.scala
@@ -1,10 +1,16 @@
+//
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-val df = sqlContext.read.format("csv").option("header", "true").load("/home/mywh/data/tran_master.csv")
+
+//
+val inputPath = "/home/mywh/data"
+val inputFileName = "tran_master.csv"
+val inputFull = inputPath + "/" + inputFileName
+val df = sqlContext.read.format("csv").option("header", "true").load(inputFull)
 
 
-val dtYear : List[String] = List("2018")
-val dtMonth : List[String] = List("03")
-val dtDay : List[String] = List("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")
+val dtYear : List[String] = List("2017")
+val dtMonth : List[String] = List("01", "02", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+val dtDay : List[String] = List("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28")
 
 for (idxYear <- 0 to (dtYear.length - 1)) {
   for (idxMonth <- 0 to (dtMonth.length - 1)) {
@@ -14,7 +20,7 @@ for (idxYear <- 0 to (dtYear.length - 1)) {
       print("\n")
       //
       val dtRange = dtYear(idxYear) + "-" + dtMonth(idxMonth) + "-" + dtDay(idxDay)
-      val outputPath = "/home/mywh/data/"
+      val outputPath = "/home/mywh/data"
       val outputFileName = "tranMaster_" + dtYear(idxYear) + dtMonth(idxMonth) + dtDay(idxDay) + ".csv"
       val outputFll = outputPath + "/" + outputFileName
       //
