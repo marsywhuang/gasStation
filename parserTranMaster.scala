457420a0b7ac7802622f26820ba2e30287393633
marsywhuang <s939703@gmail.com>
2018-10-25 16:15:55 +0800

    Update parserTranMaster.scala

--- parserTranMaster.scala
+++ parserTranMaster.scala
@@ -12,6 +12,10 @@ val dtYear : List[String] = List("2017")
 val dtMonth : List[String] = List("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
 val dtDay : List[String] = List("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28")
 
+val dtYear : List[String] = List("2017")
+val dtMonth : List[String] = List("01", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+val dtDay : List[String] = List("29", "30", "31")
+
 for (idxYear <- 0 to (dtYear.length - 1)) {
   for (idxMonth <- 0 to (dtMonth.length - 1)) {
     for (idxDay <- 0 to (dtDay.length - 1)) {
