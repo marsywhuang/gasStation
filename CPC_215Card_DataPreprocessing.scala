1cde73914208e209bdad64269f63b8d14b78c2aa
marsywhuang <s939703@gmail.com>
2018-10-25 09:13:56 +0800

    Update CPC_215Card_DataPreprocessing.scala

--- CPC_215Card_DataPreprocessing.scala
+++ CPC_215Card_DataPreprocessing.scala
@@ -16,9 +16,39 @@ val inputFull = inputPath + "/" + inputFileName
 // 讀入檔案
 val df = sqlContext.read.format("csv").option("header", "true").option("schema", "schema").load(inputFull)
 
-// 根據 CUSAUNT、CARNO 及 STDNO 進行分群
+val listColumns = List(Seq ("CUSAUNT", "CARNO", "STDNO"), 
+                       Seq("CUSAUNT", "STDNO", "CARNO"))
+
+for (idxItem <- listColumns) {
+           println(idxItem)
+           val gDf = (df.groupBy(Seq(idxItem)).sum())
+           gDf.Show()
+}
+// 車隊代碼 CusAunt－車號 CarNo－加油站代碼 StdNo－油銷量 Qty
 val gDf = (df.groupBy("CUSAUNT", "CARNO", "STDNO").
            agg(collect_list(struct("TDATE", "QTY", "MILE")).alias("Message")).
            sort("CUSAUNT", "CARNO", "STDNO"))
-           
-           
+
+// 來源目錄名稱
+val outputPath = "/home/mywh/data"
+// 來源檔案名稱
+val outputFileName = "CPC215Card_CusAunt-CarNo-StdNo-Qty.json"
+// 整體目錄及檔案名稱
+val outputFull = outputPath + "/" + outputFileName
+// 以 json 格式存檔
+gDf.write.json(outputFull)
+
+// 車隊代碼 CusAunt－加油站代碼 StdNo－車號 CarNo－油銷量 Qty
+
+// 車隊代碼 CusAunt－車號 CarNo－油銷量 Qty
+
+// 車隊代碼 CusAunt－加油站代碼 StdNo－油銷量 Qty
+// 車號 CarNo－加油站代碼 StdNo－油銷量 Qty
+
+// 加油站代碼 StdNo－車隊代碼 CusAunt－車號 CarNO－油銷量 Qty
+// 加油站代碼 StdNo－車隊代碼 CusAunt－油銷量 Qty
+// 加油站代碼 StdNo－車號 CarNO－油銷量 Qty
+
+// 車隊代碼－油銷量 Qty
+// 車號 CarNO－油銷量 Qty
+// 加油站代碼 StdNo－油銷量 Qty
