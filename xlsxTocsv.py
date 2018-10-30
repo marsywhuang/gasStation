92be3f40b94f7f61b37b2aec991ecb20b5b5da83
marsywhuang <s939703@gmail.com>
2018-10-30 13:22:06 +0800

    Update xlsxTocsv.py

--- xlsxTocsv.py
+++ xlsxTocsv.py
@@ -1,13 +1,14 @@
 
+//
 inputDirectory = "/home/mywh/data/能源局各縣市汽車加油站汽柴油銷售統計表/"
 inputFileExtension = "xlsx"
 
+outputDirectory = "home/mywh/data/output"
+
 import os
 
-for root, dirs, files in os.walk("."):  
+for root, dirs, files in os.walk(inputDirectory):  
     for filename in files:
-        print(filename)
+        if fname.endswith(inputFileExtension):
+            print(filename)
 
-for fname in os.listdir('/home/mywh/data/能源局各縣市汽車加油站汽柴油銷售統計表/107年'):
-...   if fname.endswith('.xlsx'):
-...     print(fname)
