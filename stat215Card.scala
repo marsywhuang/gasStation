69bb7fa9e4012ac435e5cb7ebb2bae901d4cbf83
marsywhuang <s939703@gmail.com>
2018-11-03 05:50:00 +0800

    Update stat215Card.scala

--- stat215Card.scala
+++ stat215Card.scala
@@ -16,17 +16,25 @@ val inputFull = inputPath + "/" + inputFileName
 // 讀入檔案
 val df = sqlContext.read.format("csv").option("header", "true").load(inputFull)
 
+// 取出 加油站代號、交易日期、量、車號、企業客戶代號
+val pDf = df.select("StdNo", "TDate", "Qty", "CarNo", "CusAUnt")
+
 // TDate 欄位屬性成為 date 型別，以及更改 Qty 欄位屬性成為 float 型別
-val tDf = (df.withColumn("TDate", to_date($"TDate", "yyyyMMdd")).
+val tDf = (pDf.withColumn("TDate", to_date($"TDate", "yyyyMMdd")).
            withColumn("Qty", df("Qty").cast(sql.types.FloatType)))
+
+
 
 // 日期級距
 val dtYear = List(List[String]("2016", "2017", "2018"))
 val dtFrom = List(List[String]("2017-01-01", "2017-02-01", "2017-03-01", "2017-04-01", "2017-05-01", "2017-06-01",
                                "2017-07-01", "2017-08-01", "2017-09-01", "2017-10-01", "2017-11-01", "2017-12-01"))
-val dtTo = List(List[String]("2017-01-31", "2017-02-28", "2017-03-31", "2017-04-30", "2017-05-31", "2017-06-31",
+val dtTo = List(List[String]("2017-01-31", "2017-02-28", "2017-03-31", "2017-04-30", "2017-05-31", "2017-06-30",
                              "2017-07-31", "2017-08-31", "2017-09-30", "2017-10-31", "2017-11-30", "2017-12-31"))
 
+
+// 來源目錄名稱
+val outputPath = "/home/mywh/data/resultData"
 
 // 取出特定日期區間的資料
 for (idx <- 0 to 11) {
@@ -35,7 +43,7 @@ for (idx <- 0 to 11) {
   val idxDtTo = dtTo(0)(idx)
   //
   val sDf = (tDf.where(($"TDate" >= idxDtFrom) && ($"TDate" <= idxDtTo)).
-             select("StdNo", "TDate", "Qty", "CarNo", "CusAUnt", "Mile"))
+             select("StdNo", "TDate", "Qty", "CarNo", "CusAUnt"))
   //
   println(idx, idxDtFrom, idxDtTo, sDf.count())
 
@@ -44,11 +52,25 @@ for (idx <- 0 to 11) {
              agg(count("StdNo") as "aStdNoTimes", sum("Qty") as "aQty").
              select("CusAUnt", "CarNo", "StdNo", "aStdNoTimes", "aQty"))
   //
-  rDf.orderBy("CusAUnt", "CarNo", "StdNo").collect().foreach(println)
+  // rDf.orderBy("CusAUnt", "CarNo", "StdNo").collect().foreach(println)
 
-  // [客戶編號, null, null, 次數, 加油總量]
-  // [客戶編號, 車號, null,  加油總量]
-  // [客戶編號, 車號, 加油站代號, 次數, 加油總量]
+  // 來源檔案名稱
+  val outputFileName = ("CusAUnt" + "CarNo" + "StdNo"
+                        + "-" + "aStdNoTimes" + "_" + "aQty" +
+                        + "_" + idxDtFrom.replace("-", "") + "-" + idxDtTo.replace("-", ""))
+  // 整體目錄及檔案名稱
+  val outputFull = outputPath + "/" + outputFileName
+  //
+  (rDf.orderBy("CusAUnt", "CarNo", "StdNo").
+       coalesce(1).
+       write.
+       format("com.databricks.spark.csv").
+       option("header", "true").
+       save(outputFull))
 
-  
+  // [null, null, null, 次數, 加油總量]：本月所有客戶的加油次數及加油總量
+  // [客戶編號, null, null, 次數, 加油總量]：某客戶的加油次數及加油總量
+  // [客戶編號, 車號, null,  次數, 加油總量]：某客戶轄下某車的加油次數及加油總量
+  // [客戶編號, 車號, 加油站代號, 次數, 加油總量]:某客戶轄下某車在某個加油站的加油次數及加油總量
+
 }
