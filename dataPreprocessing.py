870a1527b51ea03ab0b997cd63eaf0ba2b14aa38
marsywhuang <s939703@gmail.com>
2018-11-06 16:56:49 +0800

    Update dataPreprocessing.py

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,6 +1,22 @@
-#
+# 載入函式庫
+from pyspark.sql.functions import count
+from pyspark.sql.functions import countDistinct
+
+# 來源路徑
 inputPath = "/home/mywh/data/rawData"
+# 來源資料
 inputFile = "infoCpcGasStation.csv"
+# 完整路徑和資料
 inputFull = inputPath + "/" + inputFile
-#
+
+# 讀入來源資料
 df = sqlContext.read.csv(inputFull, encoding = 'utf-8', header = "true")
+
+# 
+for idxCol in range(len(df.columns)):
+  df.columns[idxCol]
+  # df.select(df[idxCol]).distinct().count()
+  # df.groupBy(df.columns[idxCol]).count().collect().foreach(println)
+  # df.where(df[idxCol]=="加盟站").count()
+  for idxRow in df.groupBy(df.columns[idxCol]).agg(count(df.columns[idxCol])).collect():
+    idxRow
