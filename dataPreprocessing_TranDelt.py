76ba9705e3761e58f655c64cfe14f02082157972
marsywhuang <s939703@gmail.com>
2018-11-09 13:02:33 +0800

    Update dataPreprocessing_TranDelt.py

--- dataPreprocessing_TranDelt.py
+++ dataPreprocessing_TranDelt.py
@@ -11,7 +11,7 @@ from pyspark.sql.functions import sum
 from pyspark.sql.functions import desc
 
 # 來源路徑
-inputPath = "/home/cpc/data/resultData/tranMaster/tranMaster_*/tranMaster_*/tranMaster_*.csv"
+inputPath = "/home/cpc/data/resultData/tranDelt/tranDelt_*/tranDelt_*/tranDelt_*.csv"
 # 來源資料
 inputFile = "p*"
 # 完整路徑和資料
@@ -21,32 +21,31 @@ inputFull = inputPath + "/" + inputFile
 df = sqlContext.read.csv(inputFull, encoding = 'utf-8', header = "false")
 
 # 表列要統計的欄位名稱
-statColumn = ['_c0', '_c3', '_c16']
+statColumn = ['_c0', '_c3', '_c9']
 # 取出特定欄位
 pDf = df.select(statColumn)
 pDf = (pDf
        .withColumnRenamed('_c0', 'StdNo')
        .withColumnRenamed('_c3', 'Date')
-       .withColumnRenamed('_c16', 'Payment'))
+       .withColumnRenamed('_c9', 'Qty'))
 #
 tDf = (pDf
        .withColumn('dateYear', pDf['Date'].substr(1, 4))
        .withColumn('dateMonth', pDf['Date'].substr(6, 2))
        .withColumn('dateDay', pDf['Date'].substr(9, 2)))
 #
-groupColumn = ['StdNo', 'Date', 'Payment', 'dateYear', 'dateMonth', 'dateDay']
-paymentColumn = ['900', '901', '902', '903', '905', '906', '907', '931', '933']
+groupColumn = ['StdNo', 'Date', 'Qty', 'dateYear', 'dateMonth', 'dateDay']
 #
 stdnoPaymentYearDf = (tDf
                       .groupBy(groupColumn[0], groupColumn[2], groupColumn[3], groupColumn[4], groupColumn[5])
-                      .agg(count(tDf.Payment.alias('aPayment')))
+                      .agg(sum(tDf.Qty.cast('float').alias('aQty')))
                       .orderBy(groupColumn[0], groupColumn[2], groupColumn[3], groupColumn[4], groupColumn[5]))
 
 
 # 路徑
 outputPath = "/home/cpc/data/resultData"
 # 資料
-outputFile = "stdnoPaymentYearMonthDayCount.json"
+outputFile = "stdnoPaymentYearMonthDaySum.json"
 # 完整路徑和資料
 outputFull = outputPath + "/" + outputFile
 #
