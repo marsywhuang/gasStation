43c6d50b5816c3d638a402f29356d75c6e197d3f
marsywhuang <s939703@gmail.com>
2018-11-14 14:24:26 +0800

    Create filloutColumnName_tranDelt.scala

---
+++ filloutColumnName_tranDelt.scala
@@ -0,0 +1,6 @@
+//
+import org.apache.spark.sql.types._
+//
+val customSchema = StructType(Array(
+  StructField("Deptno", StringType, true),
+  ,Island,Gun_No,Tran_Time,Seq,Tax_Type,Product_ID,Class,Price,Amt,Qty,Unit,Ref_No,Shift
