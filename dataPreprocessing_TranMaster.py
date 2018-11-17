1b53520f2b5a8a5863c6d60dc40bd087cbb5e322
marsywhuang <s939703@gmail.com>
2018-11-17 21:46:33 +0800

    Update dataPreprocessing_TranMaster.py

--- dataPreprocessing_TranMaster.py
+++ dataPreprocessing_TranMaster.py
@@ -9,11 +9,12 @@ from pyspark.sql.functions import count
 from pyspark.sql.functions import countDistinct
 from pyspark.sql.functions import sum
 from pyspark.sql.functions import desc
+from pyspark.sql.functions import col
+from pyspark.sql.functions import when
 
 # 來源路徑
-inputPath = "/home/cpc/data/resultData/tranMaster/tranMaster_*/tranMaster_*/tranMaster_*.csv"
+inputPath = "/home/cpc/data/tranMaster/tranMaster_*/tranMaster_*/tranMaster_*.csv"
 
-inputPath = "/home/cpc/data/tranMaster/tranMaster_2017/tranMaster_2017*/*.csv"
 # 來源資料
 inputFile = "p*"
 # 完整路徑和資料
@@ -37,23 +38,23 @@ tDf = (pDf
 # 刪除不使用欄位
 tDf = tDf.drop(tDf.Tran_time)
 
-#
+# 支付方式
 paymentColumn = ['900', '901', '902', '903', '905', '906', '907', '931', '933']
 
-# 根據加油站、年、月及日，計算各類支付方式的使用次數
-groupColumn = ['Deptno', 'Payment', 'dateYear', 'dateMonth']
-deptnoYMDaPayment = (tDf
-                     .groupBy(groupColumn)
-                     .agg(count(tDf.Payment.alias('aPayment')))
-                     .orderBy(groupColumn))
-
-# 根據加油站、年、月及日，計算各類支付方式的使用次數
-groupColumn = ['Deptno', 'Payment', 'dateYear', 'dateMonth', 'dateDay']
-deptnoYMDaPayment = (tDf
-                     .groupBy(groupColumn)
-                     .agg(count(tDf.Payment.alias('aPayment')))
-                     .orderBy(groupColumn))
-
+# 根據加油站、年及月，計算各類支付方式的使用次數
+groupColumn = ['Deptno', 'dateYear', 'dateMonth']
+deptnoYMaPayment = (tDf
+                      .groupBy(groupColumn)
+                      .agg(count(when((col("Payment") == paymentColumn[0]), True)).alias('a900'),
+                           count(when((col("Payment") == paymentColumn[1]), True)).alias('a901'),
+                           count(when((col("Payment") == paymentColumn[2]), True)).alias('a902'),
+                           count(when((col("Payment") == paymentColumn[3]), True)).alias('a903'),
+                           count(when((col("Payment") == paymentColumn[4]), True)).alias('a905'),
+                           count(when((col("Payment") == paymentColumn[5]), True)).alias('a906'),
+                           count(when((col("Payment") == paymentColumn[6]), True)).alias('a907'),
+                           count(when((col("Payment") == paymentColumn[7]), True)).alias('a931'),
+                           count(when((col("Payment") == paymentColumn[8]), True)).alias('a933'))
+                      .orderBy(groupColumn))
 
 # 路徑
 outputPath = "/home/cpc/data/resultData"
